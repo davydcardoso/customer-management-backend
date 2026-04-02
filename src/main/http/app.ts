ca05ee6c53c93a3ee09bb842af8ef6f51a6adbbe
Davyd Kewen <davydkewen@gmail.com>
2026-04-02 16:55:47 -0300

    feat: add cors allow

--- src/main/http/app.ts
+++ src/main/http/app.ts
@@ -13,7 +13,9 @@ export const createApp = () => {
   const app = express();
 
   app.use(helmet());
-  app.use(cors());
+  app.use(cors({
+    origin: '*',
+  }));
   app.use(express.json());
   app.use(
     pinoHttp({
